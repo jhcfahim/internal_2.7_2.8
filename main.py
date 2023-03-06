f6afc4ca26ff8f71c0c2a886f803a46298493e1a
Fahim Firdaus <108272834+jhcfahim@users.noreply.github.com>
2023-03-06 02:18:35 +0000

    Added lots of comments to plan how I want to code. Pseudocode

--- main.py
+++ main.py
@@ -6,3 +6,39 @@
   
   TIP: Use assessment guide to help guide you through this Internal
 '''
+
+# Welcome Message
+
+# Ask users what they want to do
+
+# If user wants to make a new file
+
+  # Ask user for name of file
+
+  # Create file
+
+  # Ask user for income before tax
+
+  # Calculate discretionary income
+
+  # While discretionary income is greater than zero loop
+
+    # Ask users to add categories and budget
+
+    # Remove budget from discretionary income
+  
+    # Store categories in file
+
+# Elif user wants to access a previous file
+
+  # Ask user for what file they want to change
+
+  # Change category or budget
+
+  # Quit the program
+
+# If user types quit
+
+  # Goodbye message
+
+  # Quit program
