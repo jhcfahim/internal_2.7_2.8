b78608d52a662e3b23a83ba5cfb35a353e9e78f1
Fahim Firdaus <108272834+jhcfahim@users.noreply.github.com>
2023-03-23 21:13:58 +0000

    added comments to functions and added spacing to make it look cleaner

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ Fahim Firdaus
 """
 import os
 
-#Functions
+# Functions
+
+
 def disposable_income(income_before_tax):
+  """Calculates disposable income and prints it out to the user."""
+
+  # Calculate the tax based on income before tax
   if income_before_tax <= 14000:
     tax = 0.105 * income_before_tax
   elif income_before_tax <= 48000:
@@ -18,13 +23,20 @@ def disposable_income(income_before_tax):
   else:
     tax = 0.39 * (income_before_tax - 180000) + 48540
 
+  # Calculates disposable income (income after tax) and rounds to 2dp
   income_after_tax = round(income_before_tax - tax, 2)
 
+  # Print the income after tax to the user
   print(f"Your income after tax is: {income_after_tax}")
+
+  # Return the income after tax value
   return income_after_tax
 
 
 def is_valid_number(input_str):
+  """Check that user has entered a float or a number."""
+
+  # Check if the input string contains only digits or a valid float number
   if input_str.isdigit() or (('.' in input_str)
                              and input_str.replace('.', '', 1).isdigit()):
     return True
@@ -33,8 +45,13 @@ def is_valid_number(input_str):
 
 
 def check_num(question):
+  """Tell user if they have not entered a valid response."""
+
+  # Loop until the user enteres a valid number
   while True:
     user_input = input(question)
+
+    # Check if the user's input is a valid number
     if is_valid_number(user_input):
       return float(user_input)
     else:
@@ -42,39 +59,66 @@ def check_num(question):
 
 
 def file_exists(file_name):
+  """Check if the file exists, and return result."""
   return os.path.isfile(file_name)
 
 
 def create_budget_file():
+    """Create a new budget file and calculate disposable income."""
+
+    # Loop to get a unique file name
     while True:
         file_name = input("\nWhat would you like the file name to be? ")
+
+        # Check if a file with that name already exists
         if not file_exists(file_name):
             break
         else:
             print("A file already exists with that name. Please choose a different name.")
+
+    # Create a new file with the given name
     with open(file_name, "w") as f:
         f.write("Monthly Budget\n")
         f.write("--------------\n")
+
+        # Loop to get the user's income before tax
         while True:
             income_before_tax = input("\nEnter your income before tax $: ")
+
+            # Check if the input is a valid number
             if is_valid_number(income_before_tax):
                 income_before_tax = float(income_before_tax)
                 break
             else:
                 print("Please enter a valid number for income.")
+
+        # Calculate the disposable income (income after tax)
         income_after_tax = disposable_income(income_before_tax)
+
+        # Add the different budget categories and their corresponding amount to the file
         add_categories(income_after_tax, f)
 
   
 def add_categories(income_after_tax, f):
+  """Allow users to add categories and budgets, and put those categories into a file."""
+
+  # Make a new dictionary to store categories.
   categories = {}
+
+  # Prompt the user to start adding categories and budgets.
   print("\nStart adding your categories and budgets below, when you want to stop, type 'quit' into the category section if you need help, type 'help' into the category section")
+
+  # Loop through until discretionary income is zero, or user types "stop"
   while income_after_tax > 0:
+   
     # Ask users to add categories and budget
     category = input("\nAdd a category: ")
+    
+    # Check if user wants to stop adding categories or needs help.
     if category == "stop":
       break
     elif category == "help":
+      # Provide examples of categories and budgets.
       print("""\nIf you need help with choosing a category, consider listing out your regular monthly expenses such as rent/mortgage, utilities, groceries, transportation, and any other bills. 
 You can also add categories for savings, entertainment, and personal expenses. 
 Once you have a list of categories, enter them one by one when prompted. 
@@ -83,10 +127,16 @@ For the budget section, enter the amount of money you want to put aside for the 
 Category: food
 Budget: 200
           """)
+
+    # If user doesn't need help, ask for the budget
     else:
       budget = check_num("Add a budget (enter a number) $: ")
+
+      # Check if the budget is more than discretionary income
       if budget > income_after_tax:
         print("Your budget is bigger than available income. Please enter a smaller budget.")
+
+      # Add category and budget to the dictionary, subtract budget from discreionary income 
       else:
         categories[category] = budget
         # Remove budget from discretionary income
@@ -95,10 +145,66 @@ Budget: 200
             print("You have used all your budget.")
             break
 
-      
+  # Write the categories and budgets into the file.    
   for category, budget in categories.items():
     f.write(f"{category}: {budget}\n\n")
-  
+
+
+def edit_budget_category():
+    """Allow users to edit already existing budgets"""
+    # Ask user for what file they want to change
+    file_edit = input("\nWhat file do you want to edit? ")
+    # Check if the file exists
+    if not os.path.isfile(file_edit):
+        print("There is no file with that name.")
+        return
+    # Open the file for editing
+    with open(file_edit, "r+") as f:
+        # Print current contents of file
+        contents = f.read()
+        print(contents)
+        # Ask user which category they want to edit
+        category_exists = False
+        while not category_exists:
+            category_to_edit = input("Which category do you want to edit? ")
+            # Find line number of category to edit
+            f.seek(0)
+            lines = f.readlines()
+            for i, line in enumerate(lines):
+                if line.startswith(category_to_edit):
+                    category_exists = True
+                    category_line_number = i
+                    category_name, category_value = line.strip().split(":")
+                    break
+            if not category_exists:
+                print("Category does not exist in the budget.")
+
+        # Ask user whether they want to change the category name or value.
+        action = ""
+        while action not in ["value", "name"]: 
+            action = input("Do you want to change the name or value of the category? ").lower()
+            if action not in ["value", "name"]:
+                print("Invalid action. Please enter 'value' or 'name'.")
+        # If user wants to change the value, prompt for new value and update.
+        if action == "value":
+            new_value = input("What is the new value for the category? ")
+            category_value = new_value
+        # If user wants to change the name, prompt for new name and update.
+        elif action == "name":
+            new_name = input("What is the new name for the category? ")
+            category_name = new_name
+
+        # Make the new line for the category and update the list of lines in the file.
+        new_line = category_name + ":" + category_value + "\n"
+        lines[category_line_number] = new_line
+
+        # Write the updated lines to the files and truncate any remaining content.
+        f.seek(0)
+        f.writelines(lines)
+        f.truncate()
+        print("\nCategory updated successfully.")
+
+      
 # Welcome Message
 print("""
 Monthly Budget Program
@@ -113,65 +219,16 @@ Quit the program (quit)
 while True:
   user_action = input("\nPlease select an action: ").lower()
   if user_action == "new":
-      create_budget_file()
+    create_budget_file()
   elif user_action == "edit":
-    # Ask user for what file they want to change
-    file_edit = input("\nWhat file do you want to edit? ")
-    if not os.path.isfile(file_edit):
-      print("There is no file with that name.")
-      continue
-    # Open file for editing
-    with open(file_edit, "r+") as f:
-      # Print current contents of file
-      contents = f.read()
-      print(contents)
-      # Ask user which category they want to edit
-      category_exists = False
-      while not category_exists:
-        category_to_edit = input("Which category do you want to edit? ")
-        # Find line number of category to edit
-        f.seek(0)
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            if line.startswith(category_to_edit):
-                category_exists = True
-                category_line_number = i
-                category_name, category_value = line.strip().split(":")
-                break
-        if not category_exists:
-          print("Category does not exist in the budget.")
-
-      action = ""
-      while action not in ["value", "name"]: 
-        action = input("Do you want to change the name or value of the category? ").lower()
-        if action not in ["value", "name"]:
-          print("Invalid action. Please enter 'value' or 'name'.")
-
-      if action == "value":
-        new_value = input("What is the new value for the category? ")
-        category_value = new_value
-
-      elif action == "name":
-        new_name = input("What is the new name for the category? ")
-        category_name = new_name
-
-      new_line = category_name + ":" + category_value + "\n"
-      lines[category_line_number] = new_line
-
-      f.seek(0)
-      f.writelines(lines)
-      f.truncate()
-      print("\nCategory updated successfully.")
-  
+    edit_budget_category()
   # Quit the program
   elif user_action == "quit":
     # Goodbye message
     print("Goodbye! Thanks for using the program.")
     break
-
   # Else user did not input a correct action
   else:
     # Prints please try again
     print("Please try again")
-
-
+    
